6e2a2cfb79d1c8831f844bd25038284f56fef955
Edi Modrić <edi.modric@gmail.com>
2017-12-16 11:20:35 +0100

    Add basic support for editing through eZ Platform Admin UI

---
+++ lib/FieldType/XmlText/FormMapper.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace eZ\Publish\Core\FieldType\XmlText;
+
+use EzSystems\RepositoryForms\Data\Content\FieldData;
+use EzSystems\RepositoryForms\FieldType\FieldValueFormMapperInterface;
+use Symfony\Component\Form\FormInterface;
+
+class FormMapper implements FieldValueFormMapperInterface
+{
+    public function mapFieldValueForm(FormInterface $fieldForm, FieldData $data)
+    {
+        $fieldForm
+            ->add(
+                $fieldForm->getConfig()->getFormFactory()->createBuilder()
+                    ->create(
+                        'value',
+                        FormType::class,
+                        array(
+                            'required' => $data->fieldDefinition->isRequired,
+                            'label' => $data->fieldDefinition->getName(),
+                        )
+                    )
+                    ->setAutoInitialize(false)
+                    ->getForm()
+            );
+    }
+}
---
+++ lib/FieldType/XmlText/FormType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace eZ\Publish\Core\FieldType\XmlText;
+
+use eZ\Publish\API\Repository\FieldTypeService;
+use EzSystems\RepositoryForms\FieldType\DataTransformer\FieldValueTransformer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class FormType extends AbstractType
+{
+    /**
+     * @var \eZ\Publish\API\Repository\FieldTypeService
+     */
+    private $fieldTypeService;
+
+    public function __construct(FieldTypeService $fieldTypeService)
+    {
+        $this->fieldTypeService = $fieldTypeService;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('xml', TextareaType::class, array('attr' => array('rows' => 10)))
+            ->addModelTransformer(
+                new FieldValueTransformer(
+                    $this->fieldTypeService->getFieldType('ezxmltext')
+                )
+            );
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'ezplatform_fieldtype_ezxmltext';
+    }
+}

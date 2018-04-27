127e6d0480f9d8c6061b171b916ea4420ddcc15f
Vidar Langseid <vl@ez.no>
2018-04-27 09:04:05 +0200

    Ensured consistent naming of variables

--- tests/lib/FieldType/Converter/RichTextTest.php
+++ tests/lib/FieldType/Converter/RichTextTest.php
@@ -14,7 +14,9 @@ use DOMXPath;
 use PHPUnit\Framework\TestCase;
 use eZ\Publish\Core\SignalSlot\Repository;
 use eZ\Publish\API\Repository\ContentService;
+use eZ\Publish\API\Repository\LocationService;
 use eZ\Publish\API\Repository\Values\Content\ContentInfo;
+use eZ\Publish\API\Repository\Values\Content\Location;
 
 class RichTextTest extends TestCase
 {
@@ -59,6 +61,25 @@ class RichTextTest extends TestCase
         return $map;
     }
 
+    /**
+     * Provider for conversion test.
+     *
+     * @return array
+     */
+    public function providerForTestTagEmbeddedImages()
+    {
+        $map = array();
+
+        foreach (glob(__DIR__ . '/_fixtures/tag_embedded_images/input/*.xml') as $inputFilePath) {
+            $basename = basename($inputFilePath, '.xml');
+            $outputFilePath = __DIR__ . "/_fixtures/tag_embedded_images/output/{$basename}.xml";
+
+            $map[] = array($inputFilePath, $outputFilePath);
+        }
+
+        return $map;
+    }
+
     protected function normalizeRewrittenIds(DOMDocument $xmlDoc)
     {
         $counter = 0;
@@ -71,6 +92,37 @@ class RichTextTest extends TestCase
         }
     }
 
+    private function createApiRepositoryStub()
+    {
+        $apiRepositoryStub = $this->createMock(Repository::class);
+        $contentServiceStub = $this->createMock(ContentService::class);
+        $locationServiceStub = $this->createMock(LocationService::class);
+        $contentInfoImageStub = $this->createMock(ContentInfo::class);
+        $contentInfoFileStub = $this->createMock(ContentInfo::class);
+        $locationStub = $this->createMock(Location::class);
+        // content with id=126 is an image, content with id=128,129 is a file
+        $map = [
+            [126, $contentInfoImageStub],
+            [128, $contentInfoFileStub],
+            [129, $contentInfoFileStub],
+        ];
+        $apiRepositoryStub->method('getContentService')
+            ->willReturn($contentServiceStub);
+        $apiRepositoryStub->method('getLocationService')
+            ->willReturn($locationServiceStub);
+        $contentServiceStub->method('loadContentInfo')
+            ->will($this->returnValueMap($map));
+
+        // image content type has id=27, file content type has id=27
+        $contentInfoImageStub->method('__get')->willReturn(27);
+        $contentInfoFileStub->method('__get')->willReturn(25);
+
+        $locationServiceStub->method('loadLocation')->willReturn($locationStub);
+        $locationStub->method('getContentInfo')->willReturn($contentInfoImageStub);
+
+        return $apiRepositoryStub;
+    }
+
     /**
      * @param string $inputFilePath
      * @param string $outputFilePath
@@ -79,27 +131,11 @@ class RichTextTest extends TestCase
      */
     public function testConvert($inputFilePath, $outputFilePath)
     {
-        $apiRepositoryStub = $this->createMock(Repository::class);
-        $contentServiceStub = $this->createMock(ContentService::class);
-        $contentInfoImageStub = $this->createMock(ContentInfo::class);
-        $contentInfoFileStub = $this->createMock(ContentInfo::class);
-        // content with id=126 is an image, content with id=128,129 is a file
-        $map = [
-            [126, $contentInfoImageStub],
-            [128, $contentInfoFileStub],
-            [129, $contentInfoFileStub]
-        ];
-        $apiRepositoryStub->method('getContentService')
-            ->willReturn($contentServiceStub);
-        $contentServiceStub->method('loadContentInfo')
-            ->will($this->returnValueMap($map));
-
-        // image content type has id=27, file content type has id=27
-        $contentInfoImageStub->method('__get')->willReturn(27);
-        $contentInfoFileStub->method('__get')->willReturn(25);
+        $apiRepositoryStub = $this->createApiRepositoryStub();
 
         $inputDocument = $this->createDocument($inputFilePath);
-        $richText = new RichText(null, $apiRepositoryStub, array(27));
+        $richText = new RichText($apiRepositoryStub);
+        $richText->setImageContentTypes(array(27));
 
         $result = $richText->convert($inputDocument, true);
 
@@ -112,6 +148,33 @@ class RichTextTest extends TestCase
         $this->assertEquals(
             $expectedDocument,
             $convertedDocument
+        );
+    }
+
+    /**
+     * @param string $inputFilePath
+     * @param string $outputFilePath
+     *
+     * @dataProvider providerForTestTagEmbeddedImages
+     */
+    public function testTagEmbeddedImages($inputFilePath, $outputFilePath)
+    {
+        $apiRepositoryStub = $this->createApiRepositoryStub();
+
+        $inputDocument = $this->createDocument($inputFilePath);
+        $richText = new RichText($apiRepositoryStub);
+        $richText->setImageContentTypes(array(27));
+
+        $richText->tagEmbeddedImages($inputDocument);
+
+        $expectedDocument = $this->createDocument($outputFilePath);
+
+        // since duplicate ids are rewritten with random values, we need to normalize those
+        $this->normalizeRewrittenIds($inputDocument);
+
+        $this->assertEquals(
+            $expectedDocument,
+            $inputDocument
         );
     }
 }
